11c512a74cd4fde3d2c174879f579ab1b1ca288f
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-30 14:54:56 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 # Autor : FanaticPythoner.
 # Please read the "LICENSE" file before doing anything.
 
-from DBFileTypes import _getAllFileTypes
 from GetFileType import FileObject
 obj = FileObject(r"SampleFile\file_example_AVI_480_750kB.avi")
 ListOfPossibleFileTypes = obj.getPossibleTypes()
