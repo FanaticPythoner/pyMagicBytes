31dadfca2e37927e3c73e054c777d4e22ef565ff
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-30 14:57:41 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@
 
 from GetFileType import FileObject
 
-#This will update the DB to the newest version from GitHub. Optional argument.
+#The 'updateDB' argument will specify to pyMagicBytes if it needs to update the DB to the newest version from GitHub. Optional argument.
 obj = FileObject(r"SampleFile\file_example_AVI_480_750kB.avi", updateDB=True)
 ListOfPossibleFileTypes = obj.getPossibleTypes()
