c013486cda572f1d1710083550d74036b996322c
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-30 15:44:07 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,5 +6,7 @@ from pyMagicBytes import FileObject
 #The 'updateDB' argument will specify to pyMagicBytes if it needs to update the DB to the newest version from GitHub. Optional argument.
 obj = FileObject(r"SampleFile\file_example_AVI_480_750kB.avi", updateDB=True)
 
-#The 'ReturnArray' argument will specify if pyMagicBytes returns an array or a nice little printable string. In this case, an array.
-ListOfPossibleFileTypes = obj.getPossibleTypes(ReturnArray=True)
+#The 'ReturnArray' argument will specify if pyMagicBytes returns an array or a nice little printable string. In this case, a string.
+ListOfPossibleFileTypes = obj.getPossibleTypes(ReturnArray=False)
+
+print(ListOfPossibleFileTypes)
