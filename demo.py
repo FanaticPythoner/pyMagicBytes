22c08df6cf6d3eba1c6e7ee3f8e2deb63617a7ea
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-30 14:58:25 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # Autor : FanaticPythoner.
 # Please read the "LICENSE" file before doing anything.
 
-from GetFileType import FileObject
+from pyMagicBytes import FileObject
 
 #The 'updateDB' argument will specify to pyMagicBytes if it needs to update the DB to the newest version from GitHub. Optional argument.
 obj = FileObject(r"SampleFile\file_example_AVI_480_750kB.avi", updateDB=True)
